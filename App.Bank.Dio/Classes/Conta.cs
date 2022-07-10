11c7e830f3ef3fbc83e546cdc9c3948068a05b3b
cleverlmf <clf.nameless@gmail.com>
2022-07-10 17:35:55 -0300

    first commit

---
+++ App.Bank.Dio/Classes/Conta.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Bank.Dio
+{
+    public class Conta
+
+    {
+        private TipoConta TipoConta { get; set; }
+        private double Saldo { get; set; }
+        private double Credito { get; set; }
+        private string Nome { get; set; }
+
+        public Conta(TipoConta tipoConta, double saldo, double credito, string nome)
+        {
+            TipoConta = tipoConta;
+            Saldo = saldo;
+            Credito = credito;
+            Nome = nome;
+        }
+
+        public bool Sacar(double valorSaque)
+        {
+            if (this.Saldo - valorSaque < (this.Credito *-1))
+            {
+                Console.WriteLine("Saldo insuficiente!");
+                return false;
+            }
+            this.Saldo -= valorSaque;
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", this.Nome, this.Saldo);
+            return true;
+        }
+
+        public void Depositar(double valorDeposito)
+        {
+            this.Saldo += valorDeposito;
+        }
+
+        public void Transferir(double valorTransferencia, Conta contaDestino)
+        {
+            if (this.Sacar(valorTransferencia))
+            {
+                contaDestino.Depositar(valorTransferencia);
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"TipoConta: {this.TipoConta} | Nome: {this.Nome} | " +
+                $"Saldo: {this.Saldo} | Credito: {this.Credito}";
+        }
+
+    }
+}
